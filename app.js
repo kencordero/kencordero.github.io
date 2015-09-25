2fd59b1a5d0a3970e59bdca097dbecb3fe184046
Kenneth Cordero <kencordero@gmail.com>
2015-09-25 15:30:07 -0500

    Add stubs and toggles for menu options;

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 var app = angular.module('app', ['ngRoute']);
 
 app.config(function ($routeProvider) {
@@ -13,28 +15,38 @@ app.config(function ($routeProvider) {
 		templateUrl: 'pages/LongNight-WebBuild.html',
 		controller: 'gameController'
 	})
-	.when('about', {
+	.when('/about', {
 		templateUrl: 'pages/about.html',
 		controller: 'aboutController'
 	})
-	.when('contact', {
+	.when('/contact', {
 		templateUrl: 'pages/contact.html',
 		controller: 'contactController'
 	})
 });
 
-app.controller('homeControler', ['$scope', function($scope) {
+app.controller('homeController', ['$scope', function($scope) {
+    $("li").removeClass("active");
+    $("#menu-home").addClass("active");
 	console.log('Home');
 }]);
 app.controller('musicController', ['$scope', function($scope) {
-	console.log('Music');
+	$("li").removeClass("active");
+    $("#menu-music").addClass("active");
+    console.log('Music');
 }]);
 app.controller('gameController', ['$scope', function($scope) {
+    $("li").removeClass("active");
+    $("#menu-game").addClass("active");
 	console.log('Game');
 }]);
 app.controller('aboutController', ['$scope', function($scope) {
+    $("li").removeClass("active");
+    $("#menu-about").addClass("active");
 	console.log('About');
 }]);
 app.controller('contactController', ['$scope', function($scope) {
+    $("li").removeClass("active");
+    $("#menu-contact").addClass("active");
 	console.log('Contact');
 }]);
