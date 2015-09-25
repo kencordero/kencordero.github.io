8118e1d912e6762fd611a421cdf206a805acb59a
Kenneth Cordero <kencordero@gmail.com>
2015-09-24 22:37:28 -0500

    Add new routing

--- app.js
+++ app.js
@@ -8,13 +8,33 @@ app.config(function ($routeProvider) {
 	.when('/music', {
 		templateUrl: 'pages/music.html',
 		controller: 'musicController'
-	})	
+	})
+	.when('/game', {
+		templateUrl: 'pages/LongNight-WebBuild.html',
+		controller: 'gameController'
+	})
+	.when('about', {
+		templateUrl: 'pages/about.html',
+		controller: 'aboutController'
+	})
+	.when('contact', {
+		templateUrl: 'pages/contact.html',
+		controller: 'contactController'
+	})
 });
 
 app.controller('homeControler', ['$scope', function($scope) {
 	console.log('Home');
 }]);
-
 app.controller('musicController', ['$scope', function($scope) {
 	console.log('Music');
+}]);
+app.controller('gameController', ['$scope', function($scope) {
+	console.log('Game');
+}]);
+app.controller('aboutController', ['$scope', function($scope) {
+	console.log('About');
+}]);
+app.controller('contactController', ['$scope', function($scope) {
+	console.log('Contact');
 }]);
