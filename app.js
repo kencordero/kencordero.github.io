24da48d80edcd65ea8e30a73d0856c967d3b2ef4
Kenneth Cordero <kencordero@gmail.com>
2015-09-25 20:26:31 -0500

    Change layout for music page

--- app.js
+++ app.js
@@ -34,11 +34,69 @@ app.controller('musicController', ['$scope', function($scope) {
 	$("li").removeClass("active");
     $("#menu-music").addClass("active");
     console.log('Music');
+    $("#videos iframe").hide();
+    
+    $("#menu-bach996-2").click(function() {
+        $("#videos iframe").hide();
+        $("#bach996-2").show().animate(1);
+    });
+    $("#menu-bach999").click(function() {
+        $("#videos iframe").hide();
+        $("#bach999").show();
+    });
+    $("#menu-guardame").click(function() {
+        $("#videos iframe").hide();
+        $("#guardame").show();        
+    });                                                                                                                         
 }]);
 app.controller('gameController', ['$scope', function($scope) {
     $("li").removeClass("active");
     $("#menu-game").addClass("active");
 	console.log('Game');
+    
+    var config = {
+	   width: 960, 
+	   height: 600,
+	   params: { enableDebugging:"0" }				
+    };
+	var u = new UnityObject2(config);
+
+	jQuery(function() {
+	   var $missingScreen = jQuery("#unityPlayer").find(".missing");
+	   var $brokenScreen = jQuery("#unityPlayer").find(".broken");
+	   $missingScreen.hide();
+	   $brokenScreen.hide();
+				
+	   u.observeProgress(function (progress) {
+	       switch(progress.pluginStatus) {
+               case "broken":
+                   $brokenScreen.find("a").click(function (e) {
+                       e.stopPropagation();
+				       e.preventDefault();
+				       u.installPlugin();
+                       return false;
+                   });
+			
+                   $brokenScreen.show();
+                   break;
+                case "missing":
+                    $missingScreen.find("a").click(function (e) {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        u.installPlugin();
+                        return false;
+                    });
+                    $missingScreen.show();
+                    break;
+                case "installed":
+                    $missingScreen.remove();
+                    break;
+                case "first":
+                    break;
+            }   
+        });
+        u.initPlugin(jQuery("#unityPlayer")[0], "LongNight-WebBuild.unity3d");
+	});	
 }]);
 app.controller('aboutController', ['$scope', function($scope) {
     $("li").removeClass("active");
