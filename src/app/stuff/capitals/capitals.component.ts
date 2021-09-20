fc0637863c05b3640ed2997a75a85188ce085467
Kenneth Cordero <kencordero@gmail.com>
2021-09-20 17:44:19 -0500

    Move quiz logic to its own service

--- src/app/stuff/capitals/capitals.component.ts
+++ src/app/stuff/capitals/capitals.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { countries } from '../../shared/models/countries';
+import { Country } from '../models/country.model';
+import { Question } from '../models/question.model';
+import { QuizService } from '../services/quiz.service';
 
 const CHOICE_COUNT = 9;
 
@@ -9,38 +11,21 @@ const CHOICE_COUNT = 9;
   styleUrls: ['./capitals.component.css']
 })
 export class CapitalsComponent implements OnInit {
-  countries: any[] = countries;
-  question: any = {};
+  question: Question;
   result: boolean | undefined;
-  constructor() { }
+  constructor(private quizService: QuizService) { 
+    this.question = this.quizService.setupQuestion();
+  }
 
   ngOnInit(): void {
-    this.setupQuestion();
   }
 
-  setupQuestion() {
+  onNextQuestion() {
     this.result = undefined;
-    const randomCountryId = Math.floor(Math.random() * countries.length);
-    this.question.country = countries[randomCountryId].name;
-    this.question.correctAnswer = countries[randomCountryId].capital;
-    let wrongAnswers: any[] = countries.filter(c => c.name !== this.question.country);
-    this.shuffle(wrongAnswers);
-    wrongAnswers = wrongAnswers.slice(0, CHOICE_COUNT - 1);
-    wrongAnswers.unshift(countries[randomCountryId]);
-    this.shuffle(wrongAnswers);
-    this.question.options = wrongAnswers;
-  }
+    this.question = this.quizService.setupQuestion();
+  }  
 
-  shuffle(array: any[]): void {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-  }
-
-  checkResponse(option: any): void {
-    this.result = option.capital === this.question.correctAnswer;
+  onOptionSelected(option: Country) {
+    this.result = this.quizService.checkResponse(option);
   }
 }
