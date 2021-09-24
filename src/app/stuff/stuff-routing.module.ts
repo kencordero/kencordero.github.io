79758e58b88845fffe64cbc91155f7e86b84609d
Kenneth Cordero <kencordero@gmail.com>
2021-09-24 17:18:30 -0500

    add new component for math

--- src/app/stuff/stuff-routing.module.ts
+++ src/app/stuff/stuff-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArithmeticComponent } from './arithmetic/arithmetic.component';
 import { CapitalsComponent } from './capitals/capitals.component';
 
 const routes: Routes = [
-  { path: 'capitals', component: CapitalsComponent }
+  { path: 'capitals', component: CapitalsComponent },
+  { path: 'arithmetic', component: ArithmeticComponent }
 ];
 
 @NgModule({
--- src/app/stuff/stuff.module.ts
+++ src/app/stuff/stuff.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StuffRoutingModule } from './stuff-routing.module';
 import { QuizService } from './services/quiz.service';
+import { ArithmeticComponent } from './arithmetic/arithmetic.component';
+import { ArithmeticService } from './services/arithmetic.service';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ArithmeticComponent
+  ],
   imports: [
     CommonModule,
     StuffRoutingModule
   ],
   providers: [ 
-    QuizService
+    QuizService,
+    ArithmeticService
   ],
  
 })
