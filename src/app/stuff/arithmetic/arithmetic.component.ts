b9b77807cccaba66e97f054d166b0e83929fabb7
Kenneth Cordero <kencordero@gmail.com>
2021-09-25 01:21:17 -0500

    Implement math component

--- src/app/stuff/arithmetic/arithmetic.component.ts
+++ src/app/stuff/arithmetic/arithmetic.component.ts
@@ -6,10 +6,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./arithmetic.component.css']
 })
 export class ArithmeticComponent implements OnInit {
+  correctAnswer?: number;
+  number1?: number;
+  number2?: number;
+  isCorrect?: boolean;
+  operator: string;
+  operatorSymbol?: string;
+  response?: string;
+  
+  constructor() {
+    this.operator = 'addition';
+    this.setupQuestion();    
+  }
 
-  constructor() { }
+  setupQuestion() {
+    this.response = undefined;
+    this.isCorrect = undefined;
+    let max: number;
+    switch (this.operator) {
+      case 'addition':
+        this.operatorSymbol = '+'
+        max = 100;
+        break;
+      case 'multiplication':
+        this.operatorSymbol = 'x';
+        max = 12;
+        break;
+      default:
+        max = 0;
+        break;
+    }
+    this.number1 = this.createRandomNumber(1, max);
+    this.number2 = this.createRandomNumber(1, max);
+
+    switch (this.operator) {
+      case 'addition':
+        this.correctAnswer = this.number1 + this.number2;
+        break;
+      case 'multiplication':
+        this.correctAnswer = this.number1 * this.number2;
+        break;
+    }
+    
+  }
+
+  createRandomNumber(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
 
   ngOnInit(): void {
   }
 
+  checkResponse(e: KeyboardEvent) {
+    if (e.key !== 'Enter') return;
+    this.isCorrect = this.response == this.correctAnswer;
+    console.log(this.isCorrect);
+    setTimeout(() => this.setupQuestion(), 1500);
+  }
+
+  onOperatorChanged(e: any) {
+    this.setupQuestion();
+  }
 }
--- src/app/stuff/stuff.module.ts
+++ src/app/stuff/stuff.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+
 import { StuffRoutingModule } from './stuff-routing.module';
 import { QuizService } from './services/quiz.service';
 import { ArithmeticComponent } from './arithmetic/arithmetic.component';
 import { ArithmeticService } from './services/arithmetic.service';
+import { CapitalsComponent } from './capitals/capitals.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
 
 
 @NgModule({
   declarations: [
-    ArithmeticComponent
+    ArithmeticComponent,
+    CapitalsComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonToggleModule,
     StuffRoutingModule
   ],
   providers: [ 
