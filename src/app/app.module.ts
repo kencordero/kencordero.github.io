b90ef06e204b91253b1ff14df4ea602400edc9ea
Kenneth Cordero <kencordero@gmail.com>
2021-12-04 23:51:16 -0600

    Use native Angular YouTubePlayer module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,22 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
-import { SafePipeModule } from 'safe-pipe';
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { MusicComponent } from './music/music.component';
 import { MainComponent } from './main/main.component';
+import { VideoPlayerComponent } from './shared/components/video-player/video-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     MusicComponent,
-    MainComponent
+    MainComponent,
+    VideoPlayerComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,7 @@ import { MainComponent } from './main/main.component';
     HttpClientModule,
     MatButtonModule,
     MatMenuModule,
-    SafePipeModule,
+    YouTubePlayerModule,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/music/videos.ts
+++ src/app/music/videos.ts
@@ -1,14 +1,14 @@
 export const videos = [
     {
         title: "J.S. Bach - Allemande from BWV 996",
-        url: "https://www.youtube.com/embed/UUbqdJDEoM4"
+        videoId: "UUbqdJDEoM4"
     },
     {
         title: "J.S. Bach - Prelude, BWV 999",
-        url: "https://www.youtube.com/embed/BDo5dEkTTUw"
+        videoId: "BDo5dEkTTUw"
     },
     {
         title: "Narvaez - Guardame las vacas",
-        url: "https://www.youtube.com/embed/qy1ccbpFoTI"
+        videoId: "qy1ccbpFoTI"
     }
 ]
