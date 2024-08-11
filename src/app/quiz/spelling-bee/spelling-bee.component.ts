280e48bc67033bc46139426c9597fa81d395484d
Kenneth Cordero <kencordero@gmail.com>
2024-08-11 10:44:09 -0500

    Spelling bee updates

--- src/app/quiz/spelling-bee/spelling-bee.component.ts
+++ src/app/quiz/spelling-bee/spelling-bee.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import SpellingBeeService from '../../services/spelling-bee.service';
-import { shuffle } from 'src/app/shared/utils';
+import { fromEvent, map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-spelling-bee',
@@ -11,43 +11,54 @@ export class SpellingBeeComponent implements OnInit {
   public word?: string;
   public dictionaryEntry: any;
   public definitions: any;
-  private voices: SpeechSynthesisVoice[];
+  private voices: SpeechSynthesisVoice[] = [];
+  public response: string | undefined;
+  public voices$:  Observable<SpeechSynthesisVoice[]>;
 
-  constructor(private api: SpellingBeeService) { 
-    const synth = window.speechSynthesis;
-    console.log('Synth: ' + synth);
-    this.voices = synth.getVoices();
-    console.log('Voices: ' + this.voices);
+  constructor(private api: SpellingBeeService) {
+    this.voices$ = fromEvent(speechSynthesis, 'voiceschanged')
+    .pipe(
+       map(() => speechSynthesis.getVoices().filter(voice => voice.lang.includes('en'))),
+       tap((voices) => this.voices = voices),
+    );
   }
 
   ngOnInit(): void {
-    this.onClickNext();
+    this.voices$.subscribe({
+      next: () => {
+        console.log('voices', this.voices);
+        this.onClickNext();
+      }
+    });
   }
 
   onClickNext(): void {
+    this.response = '';
     this.word = this.api.getNextWord();
     console.log('Word: ' + this.word);
     
     this.speak();
-    
-    // this.dictionaryEntry = null;
-    // this.definitions = null;
-    
-    // this.api.getDictionaryEntry(this.word).subscribe({
-    //   next: data => {
-    //     this.dictionaryEntry = data;
-    //     this.definitions = this.api.extractDefinitions(data);
-    //   },
-    //   error: () => {
-    //     console.log('Could not find definition for ' + this.word);
-    //   }
-    // });
   }
 
   speak(): void {
-    let utterance = new SpeechSynthesisUtterance(this.word);
-    utterance.voice = this.voices[0];
-    
+    const utterance = new SpeechSynthesisUtterance(this.word);
+    utterance.rate = 0.8;
+    const voice = this.voices.find(voice => voice.name === 'Google US English');
+    if (voice) {
+      utterance.voice = voice
+    }
     speechSynthesis.speak(utterance);
+  }
+
+  checkResponse(e: any): void {
+    if (e.key !== 'Enter') return;
+    
+    //this.isCorrect = this.response === this.correctAnswer;
+    setTimeout(() => this.onClickNext(), 1500);
+    if (this.response?.toLowerCase() === this.word?.toLowerCase()) {
+      console.log('Correct!');
+    } else {
+      console.log('Incorrect!');
+    }
   }
 }
